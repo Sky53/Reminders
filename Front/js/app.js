c86668afc5d7cfde21991fa9e341b61fc81d2ee1
Viacheslavchik <vviacheslav93@gmail.com>
2020-06-02 16:37:27 +0300

    Добавлена форма авторизации и настроены сценарии ошибок к ней, принимает любые валидные значения. Небольшое изменение внешнего вида. Убран лишний код, продолжается работа над уведомлениями.

--- Front/js/app.js
+++ Front/js/app.js
@@ -13,12 +13,14 @@ Vue.component('todo-item',{
 new Vue({
 	el: '#app',
 	data: {
-		NewTaskName:null,
+		errors:[],
 		logs: [],
+		NewTaskName:null,
 		NewTaskDescription:null,
 		NewTaskDate:null,
 		NewTaskComments:null,
-		errors:[],
+		userLogin: null,
+		userPassword: null,
 		newTask: '',
 		tasks: [
 			{name:'Поход к врачу',description: 'Врач хирург',dataTask: '05.05.2020',comments: ''},
@@ -27,6 +29,9 @@ new Vue({
 				],
 		selectedTaskIndex: '',
 		cardVisibility: false,
+		formautoriz: true,
+		nameform: false,
+		btform: false,
 		search: '',
 		jnlVisibility: false,
 		FormNewTask: false
@@ -37,33 +42,63 @@ new Vue({
 			this.task = this.filteredTasks[index]
 		},
 
+		checkForm: function (e) {
+		      this.errors = [];
+		      if (!this.userLogin) {
+		        this.errors.push('Укажите логин.');
+		      }
+		      if (!this.userPassword) {
+		        this.errors.push('Укажите пароль.');
+		      } else if (!this.validLogin(this.userLogin)) {
+		        this.errors.push('Некорректный логин.');
+		      }
+
+		      if (!this.errors.length) {
+						this.formautoriz = false;
+						this.btform = true;
+						this.nameform = true;
+		        return true;
+		      }
+		      e.preventDefault();
+		    },
+		    validLogin: function (userLogin) {
+		      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		      return re.test(userLogin);
+		    },
 
 
-		makeNewTask: function(e) {
+		makeNewTask: function(f) {
+			if (this.NewTaskName && this.NewTaskDescription && this.NewTaskDate)
+				{
+					this.tasks.push({
+					name: this.NewTaskName,
+					description: this.NewTaskDescription,
+					dataTask: this.NewTaskDate,
+					comments: this.NewTaskComments,
 
-			this.tasks.push({
-				name: this.NewTaskName,
-				description: this.NewTaskDescription,
-				dataTask: this.NewTaskDate,
-				comments: this.NewTaskComments,
-			})
+					})
+					this.NewTaskName='',
+					this.NewTaskDescription='',
+					this.NewTaskDate='',
+					this.NewTaskComments='',
+					this.logs.push(
+						log(`Вы создали событие: ${this.NewTaskName}`, 'ok')
+				)
+			}
+					else {
+						if(!this.NewTaskName){ this.errors.push("Название.");}
+						if(!this.NewTaskDescription){ this.errors.push("Описание.");}
+						if(!this.NewTaskDate){ this.errors.push("Дата.");}
+						this.errors = []
+					f.preventDefault()}
+			},
 
-			if(this.NewTaskName && this.NewTaskDescription && this.NewTaskDate) return true;
-			      this.errors = [];
-			      if(!this.NewTaskName) this.errors.push("Название.");
-			      if(!this.NewTaskDescription) this.errors.push("Описание.");
-						if(!this.NewTaskDate) this.errors.push("Дата.");
-			      e.preventDefault();
-		},
 
-
-
-
-		newOrder: function(){
-			this.logs.push(
-				log(`Вы создали событие: ${this.NewTaskName}`, 'ok')
-			)
-		},
+newOrder: function(){
+this.logs.push(
+log(`Вы создали событие: ${this.NewTaskName}`, 'ok')
+)
+},
 		deleteOrder: function(){
 			this.logs.push(
 				log(`Вы удалили событие: ${this.task.name}`, 'cnl')
