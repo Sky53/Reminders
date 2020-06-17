81458937ad78db52a70b6f4719f299c8a09cc01d
Viacheslavchik <vviacheslav93@gmail.com>
2020-06-17 15:22:23 +0300

    Первичная адаптация для малых диагоналей экранов, небольшие доработки по функционалу, новый вид уведомлений действий.

--- Front/js/app.js
+++ Front/js/app.js
@@ -1,20 +1,22 @@
 const log = (text, type) => ({text, type})
 
-
-Vue.component('todo-item',{
-	template: '\
+Vue.component('todo-item', {
+	template:
+	'\
     <li>\
       {{ name }} {{ description }} {{dataTask}} {{comments}}\
     </li>\
-  ',
- props: ['name','description','dataTask','comments']
-})
-
+		',
+ props: ['name','description','dataTask','comments'],
+}
+)
 new Vue({
 	el: '#app',
 	data: {
+		styleAlert: {backgroundColor: 'lightgreen'},
 		errors:[],
 		logs: [],
+		fails:[],
 		NewTaskName:null,
 		NewTaskDescription:null,
 		NewTaskDate:null,
@@ -25,7 +27,7 @@ new Vue({
 		tasks: [
 			{name:'Поход к врачу',description: 'Врач хирург',dataTask: '05.05.2020',comments: ''},
 			{name:'Позвонить маме',description: 'Номер +7(+++)+++++++',dataTask: '07.05.2020',comments: ''},
-			{name:'Туса в баре', description:'Бухич',dataTask: '10.05.2020', comments:''}
+			{name:'Поход в бар', description:'Посиделки',dataTask: '10.05.2020', comments:''}
 				],
 		selectedTaskIndex: '',
 		cardVisibility: false,
@@ -36,13 +38,14 @@ new Vue({
 		jnlVisibility: false,
 		FormNewTask: false
 	},
-	methods: {
+
+methods: {
 		selectTask: function(index) {
 			this.selectedTaskIndex = index
 			this.task = this.filteredTasks[index]
 		},
 
-		checkForm: function (e) {
+checkForm: function (e) {
 		      this.errors = [];
 		      if (!this.userLogin) {
 		        this.errors.push('Укажите логин.');
@@ -52,7 +55,6 @@ new Vue({
 		      } else if (!this.validLogin(this.userLogin)) {
 		        this.errors.push('Некорректный логин.');
 		      }
-
 		      if (!this.errors.length) {
 						this.formautoriz = false;
 						this.btform = true;
@@ -60,64 +62,57 @@ new Vue({
 		        return true;
 		      }
 		      e.preventDefault();
-		    },
-		    validLogin: function (userLogin) {
+},
+
+
+validLogin: function (userLogin) {
 		      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		      return re.test(userLogin);
-		    },
-
-
-		makeNewTask: function(f) {
-			if (this.NewTaskName && this.NewTaskDescription && this.NewTaskDate)
-				{
-					this.tasks.push({
-					name: this.NewTaskName,
-					description: this.NewTaskDescription,
-					dataTask: this.NewTaskDate,
-					comments: this.NewTaskComments,
-
-					})
-					this.NewTaskName='',
-					this.NewTaskDescription='',
-					this.NewTaskDate='',
-					this.NewTaskComments='',
-					this.logs.push(
-						log(`Вы создали событие: ${this.NewTaskName}`, 'ok')
-				)
-			}
-					else {
-						if(!this.NewTaskName){ this.errors.push("Название.");}
-						if(!this.NewTaskDescription){ this.errors.push("Описание.");}
-						if(!this.NewTaskDate){ this.errors.push("Дата.");}
-						this.errors = []
-					f.preventDefault()}
-			},
-
-
-newOrder: function(){
-this.logs.push(
-log(`Вы создали событие: ${this.NewTaskName}`, 'ok')
-)
 },
-		deleteOrder: function(){
+
+makeNewTask: function(f) {
+				      this.fails = [];
+				      if(!this.NewTaskName)
+				        { this.fails.push("Название");}
+				      if(!this.NewTaskDescription)
+				        { this.fails.push("Описание");}
+				      if(!this.NewTaskDate)
+				        { this.fails.push("Дата");}
+				      if (!this.fails.length) {
+				      this.tasks.push({
+				      name: this.NewTaskName,
+				      description: this.NewTaskDescription,
+				      dataTask: this.NewTaskDate,
+				      comments: this.NewTaskComments,
+				      })
+								f.preventDefault()
+							this.logs.push(
+									log(`Вы создали событие: ${this.NewTaskName}`)
+							)
+				      this.NewTaskName='',
+				      this.NewTaskDescription='',
+				      this.NewTaskDate='',
+				      this.NewTaskComments='',
+							styleAlert='false'
+				  }
+},
+
+deleteOrder: function(selectedTaskIndex){
+	 this.$delete(this.tasks, selectedTaskIndex);
 			this.logs.push(
-				log(`Вы удалили событие: ${this.task.name}`, 'cnl')
-			)
+				log(`Вы удалили событие: ${this.task.name}`)
+										)
+				cardVisibility='false'
 		}
+},
 
-	},
-
-	computed: {
+computed: {
 		filteredTasks() {
 			var searchTemplate = this.search.toLowerCase();
 				return this.tasks.filter((task) => {
 				return task.name.toLowerCase().includes(searchTemplate) || task.description.toLowerCase().includes(searchTemplate)
 			})
-		}
-	},
-	filters: {
-		NTDate(value) {
-			return value.toLocaleString()
-		}
-	}
+										}
+},
+
 })
